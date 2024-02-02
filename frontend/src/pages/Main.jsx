f6793983d01e05b2bbe155c0254dcb066ad2421b
ksh0123 <shnae.k@gmail.com>
2024-02-02 21:10:10 +0900

    [Add] LPToken component

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import NetworkButton from "../components/NetworkButton";
 import NetworkSwitch from "../components/NetworkSwitch";
+import LPTokens from "../components/LPTokens";
 
 const Main = () => {
   const { isNetworkButtonClick } = useOutletContext();
@@ -49,31 +50,7 @@ const Main = () => {
           <div>$24.02</div>
         </div>
       </div>
-      <div
-        className={`bg-neutral-400 rounded-lg w-11/12 h-fit pb-10 my-10 flex flex-col ${
-          isNetworkButtonClick ? "bg-opacity-30" : "bg-neutral-400"
-        }`}
-      >
-        <div className="flex flex-row justify-between m-4">
-          <div>UNISWAP POOL</div>
-          <div>See Profit</div>
-        </div>
-        <div className="flex flex-row justify-between mx-4 text-neutral-200">
-          <div>PROVIDED</div>
-          <div>AMOUNT</div>
-          <div>USD</div>
-        </div>
-        <div className="flex flex-row justify-between mx-4 text-neutral-50">
-          <div>MATIC</div>
-          <div>39</div>
-          <div>$29.10</div>
-        </div>
-        <div className="flex flex-row justify-between mx-4  text-neutral-50">
-          <div>USDT</div>
-          <div>19</div>
-          <div>$19.48</div>
-        </div>
-      </div>
+      <LPTokens />
     </div>
   );
 };
