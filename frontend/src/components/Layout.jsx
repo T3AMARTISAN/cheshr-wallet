0b85836337931c8a735b60249bce578b434a6d9b
jihwan2843 <jihwan2843@gmail.com>
2024-02-02 15:28:44 +0900

    Swtich Network complete

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,12 +4,23 @@ import { useState } from "react";
 
 const Layout = () => {
   const [isNetworkButtonClick, setIsNetworkButtonClick] = useState();
+  const [currentProvider, setCurrentProvider] = useState();
+  const [currentNetwork, setCurrentNetwork] = useState();
 
   return (
     <>
       <div className="bg-neutral-100">
         <div className="mx-auto">
-          <Outlet context={{ isNetworkButtonClick, setIsNetworkButtonClick }} />
+          <Outlet
+            context={{
+              isNetworkButtonClick,
+              setIsNetworkButtonClick,
+              currentProvider,
+              setCurrentProvider,
+              currentNetwork,
+              setCurrentNetwork,
+            }}
+          />
         </div>
       </div>
       <Menu />
--- frontend/src/components/NetworkSwitch.jsx
+++ frontend/src/components/NetworkSwitch.jsx
@@ -1,43 +1,48 @@
 import { useOutletContext } from "react-router-dom";
+import { ethers } from "ethers";
+import Ethereum from "./Ethereum";
+import Polygon from "./Polygon";
 
 const NetworkSwitch = () => {
-  const { setIsNetworkButtonClick } = useOutletContext();
+  const { setIsNetworkButtonClick, setCurrentProvider } = useOutletContext();
 
   const onClickClose = () => {
     setIsNetworkButtonClick(false);
+  };
+
+  const onClickLinea = () => {
+    setCurrentProvider();
+  };
+
+  const onClickAvalanche = () => {
+    setCurrentProvider();
   };
 
   return (
     <div className="bg-white absolute w-full h-[480px] rounded-lg">
       <div className="h-3/5 flex flex-col justify-center pl-3 gap-2">
         <div>Main Network</div>
-        <div className="border rounded-lg border-black py-1 px-1 mr-1">
-          Ethereum Mainnet
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
-          Polygon Mainnet
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
+        <Ethereum />
+        <Polygon />
+        <button
+          className="border rounded-lg border-black py-1 pl-1 mr-1"
+          onClick={onClickLinea}
+        >
           Linea Mainnet
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
-          Optimism Mainnet
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
-          Arbitrum Mainnet
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
+        </button>
+        <Optimism />
+        <Arbitrum />
+        <button
+          className="border rounded-lg border-black py-1 pl-1 mr-1"
+          onClick={onClickAvalanche}
+        >
           Avalanche C-Chain
-        </div>
+        </button>
       </div>
       <div className="bg-blue-100 h-2/5 flex flex-col justify-center pl-3 gap-2">
         <div>Test Network</div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
-          ETH Sepolia Test Network
-        </div>
-        <div className="border rounded-lg border-black py-1 pl-1 mr-1">
-          ETH Sepolia Test Network
-        </div>
+        <Sepolia />
+        <Goerli />
         <button className="mt-2" onClick={onClickClose}>
           X
         </button>
