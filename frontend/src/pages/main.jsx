e6df3dbdc4dddca9cefcca97415c96021b12afbe
ksh0123 <shnae.k@gmail.com>
2024-02-03 16:13:54 +0900

    temp commit

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -1,32 +1,77 @@
 import { useOutletContext } from "react-router-dom";
-import Assets from "../components/Tokens";
-import LpPoolCard from "../components/LpPoolCard";
-import TabBar from "../components/TabBar";
-import TotalAsset from "../components/TotalAsset";
-import WalletAddress from "../components/WalletAddress";
-import DeFi from "../components/DeFi";
-import Tokens from "../components/Tokens";
+import NetworkButton from "../components/NetworkButton";
 import NetworkSwitch from "../components/NetworkSwitch";
 
 const Main = () => {
-  const { tabNumber, setTabNumber, isNetworkButtonClick } = useOutletContext();
+  const { isNetworkButtonClick } = useOutletContext();
 
   return (
-    //전체 컨테이너 - 모바일화면 크기
-    <div className=" container  bg-red-100  overflow-y-auto ">
-      <div className="bg-yellow-100 ">
-        {/* user wallet address */}
-        <div className="sticky top-0 ">
-          <WalletAddress />
-          {isNetworkButtonClick ? <NetworkSwitch /> : ""}
-        </div>
-        {/* total asset block */}
-        <TotalAsset />
-        <div className="my-3">
-          {tabNumber == 0 ? <Tokens /> : ""}
-          {tabNumber == 1 ? <DeFi /> : ""}
+    <div
+      className={` w-[480px] h-screen mx-auto flex flex-col items-center ${
+        isNetworkButtonClick ? "bg-opacity-30" : "bg-neutral-300"
+      }`}
+    >
+      <div className="text-neutral-900 font-extrabold text-7xl mb-6">
+        DEX WALLET
+      </div>
 
-          {/* lp pool block */}
+      <div className="bg-neutral-500 rounded-lg w-11/12 h-1/3 mb-6 whitespace-pre flex justify-between relative">
+        <div className="text-neutral-50 font-light m-6 text-2xl">
+          {"February 29, 2024\n$100"}
+        </div>
+        <NetworkButton />
+        {isNetworkButtonClick ? <NetworkSwitch /> : ""}
+      </div>
+
+      <div className="bg-neutral-400 rounded-lg w-11/12 h-fit pb-10 flex flex-col">
+        <div className="flex flex-row justify-between m-4">
+          <div>ASSETS</div>
+          <div>See Profit</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4  text-neutral-200">
+          <div>ASSET/AMOUNT </div>
+          <div>PRICE</div>
+          <div>USD</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4  text-neutral-50">
+          <div>USDT / 24.0379</div>
+          <div>$0.9992</div>
+          <div>$24.02</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4  text-neutral-50">
+          <div>USDT / 24.0379</div>
+          <div>$0.9992</div>
+          <div>$24.02</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4  text-neutral-50">
+          <div>USDT / 24.0379</div>
+          <div>$0.9992</div>
+          <div>$24.02</div>
+        </div>
+      </div>
+      <div
+        className={`bg-neutral-400 rounded-lg w-11/12 h-fit pb-10 my-10 flex flex-col ${
+          isNetworkButtonClick ? "bg-opacity-30" : "bg-neutral-400"
+        }`}
+      >
+        <div className="flex flex-row justify-between m-4">
+          <div>UNISWAP POOL</div>
+          <div>See Profit</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4 text-neutral-200">
+          <div>PROVIDED</div>
+          <div>AMOUNT</div>
+          <div>USD</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4 text-neutral-50">
+          <div>MATIC</div>
+          <div>39</div>
+          <div>$29.10</div>
+        </div>
+        <div className="flex flex-row justify-between mx-4  text-neutral-50">
+          <div>USDT</div>
+          <div>19</div>
+          <div>$19.48</div>
         </div>
       </div>
     </div>
