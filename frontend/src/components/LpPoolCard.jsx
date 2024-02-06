8705a12d47b95fb39aea1da26c71f78bbc186f77
cryptojang <robert.crypto.jang@gmail.com>
2024-02-06 15:33:54 +0900

    lp token dollar value plan

--- frontend/src/components/LpPoolCard.jsx
+++ frontend/src/components/LpPoolCard.jsx
@@ -17,11 +17,10 @@ const LPPoolCard = () => {
         const etherscanKey = process.env.REACT_APP_ETHERSCAN_API_KEY;
         const infuraKey = process.env.INFURA_API_KEY;
         const testAccount = process.env.REACT_APP_TEST_ACCOUNT;
-        const pairAddress = "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852";
-        setPairAddress(pairAddress);
-
         // WETH-USDT Pair LP Token Contract
         // CA: 0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852
+        const pairAddress = "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852";
+        setPairAddress(pairAddress);
 
         //사용자가 보유한 LP 토큰 개수
         const token_url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${pairAddress}&address=${testAccount}&tag=latest&apikey=${etherscanKey}`;
@@ -44,7 +43,55 @@ const LPPoolCard = () => {
         const contract_symbol = await contract.symbol();
         setLPTokenName(contract_symbol);
 
-        // 아래 추가된 부분은 사용자가 보유한 LP 토큰의 달러 가치 계산하기
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        /* 
+        ####사용자가 보유한 LP 토큰의 달러 가치 계산하기####
+        : LP CA 주소를 인풋으로 받아서 사용자 보유 LP 토큰 달러 가치 반환하는 함수
+
+          - https://v2.info.uniswap.org/pairs 유니스왑 v2 상위 페어  
+          - 0. ###lp CA는 사용자가 입력하게 둔다고 가정###
+        */
+
+        /* 1. lp CA를 받아서 두 페어 파악: lp CA의 token0, token1 함수 활용
+         */
+
+        /* 2. 두 토큰 페어의 유형 파악: token0과 token1 중에 스테이블코인 있는지, ETH 있는지, 스테이블코인과 ETH 둘 다 없는지 세 경우로 나뉨
+          -###usdt 등 유명 토큰의 ca 저장한 json 파일 필요
+          -usdt, usdc, dai, fei, eth(WETH ca) 주소 미리 저장해놓고, 
+          -비교해서 유형 파악해서 분류 (분류 후 if 문으로 각각의 아래 함수 호출)
+        */
+
+        /*3. token0 이나 token1 중 하나가 스테이블코인(usdt, usdc, dai, fei 등) 인 경우 lp 토큰 달러 가치 반환 함수
+          -tokenA를 스테이블코인 페어로 지정하고, tokenB를 그외 페어로 지정
+          -tokenA의 잔고 파악해 Lp CA 의 TVL 계산 (lp CA의 getReserves() 하면 두 페어의 잔고 나옴 -> 굳이 해당 토큰 ca 안 가도 이 컨에서 잔고 확인 가능)
+          -Total LpSupply 계산
+          -사용자의 LpAmount 계산
+          - 위의 값으로 LP 토큰의 달러 가치 계산        
+        */
+
+        /*4. token0과 token1 모두 스테이블코인이 아닐 때 lp 토큰 달러 가치 반환 함수 : 둘 중 하나가 ETH인 경우
+          -tokenA를 ETH로 지정, tokenB를 나머지 페어로 지정
+          -tokenA의 잔고 파악 (lp CA의 getReserves() 하면 두 페어의 잔고 나옴 -> 굳이 해당 토큰 ca 안 가도 이 컨에서 잔고 확인 가능)
+          -tokenA의 시세 DEX에서 가져오기(twap 활용한 24시간 평균 가격?: // https://soliditydeveloper.com/uniswap-oracle ca에서 시간가중평균 가격 가져오기 관련)
+          -tokenA의 잔고와 시세로 Lp CA의 TVL 계산
+          -Total LpSupply 계산
+          -사용자늬 LpAmount 계산
+          -위의 값으로 Lp 토큰 달러 가치 계산
+        */
+
+        /*5. token0과 token1 모두 스테이블코인도 아니고 ETH도 아닌 경우: uniswap v2 pool 상위 100위권에는 없어서 일단 보류
+         */
+
+        /*에러 발생 우려 있는 부분(조사 필요): useEffect 와 async 함수. 
+        getTokens() 안에 모든 걸 넣어야 할지,
+        각각의 함수로 분류한 후 연이어 호출하게 할지, (이 경우 useEffect 어떻게 구성해야 잘 작동할지)
+        */
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        /*
+          아래는 그 전에 WETH-USDT 페어로 LP 토큰의 달러 가치 계산했던 기존 코드
+        */
 
         //1. LP Contract에 있는 전체 USDT 개수 구하기 (LP의 TVL = 2 * 전체 USDT 개수)
         const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
