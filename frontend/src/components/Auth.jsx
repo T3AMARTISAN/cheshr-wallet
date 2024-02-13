2559bc033e4e52fc0f3bde925af040f9a3998875
ksh0123 <shnae.k@gmail.com>
2024-02-13 13:04:13 +0900

    [MODIFY] AuthProvider checks currentAccount status

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -9,20 +9,23 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const isAuthenticated = checkAuthentication();
-    const hasWallet = checkWallet();
-    // if (!isAuthenticated && hasWallet) {
-    //   navigate("/unlock");
-    // }
+    // const hasWallet = checkWallet();
+
+    // currentAccount가 비어있는 경우 - 잠금 또는 지갑이 없는 상태
+    // 홈화면으로 이동한다
+    if (!isAuthenticated) {
+      navigate("/");
+    }
   });
 
   const checkAuthentication = () => {
     return !!currentAccount;
   };
 
-  const checkWallet = () => {
-    const wallet = localStorage.getItem("dexwalletData");
-    return !!wallet;
-  };
+  // const checkWallet = () => {
+  //   const wallet = localStorage.getItem("dexwalletData");
+  //   return !!wallet;
+  // };
 
   return (
     <AuthContext.Provider value={{ currentAccount, setCurrentAccount }}>
