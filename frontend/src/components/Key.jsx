1021b716013a94bcd9bdf56c1839edc743dbd06c
ksh0123 <shnae.k@gmail.com>
2024-02-16 20:35:29 +0900

    [ADD] Mount existing wallet with private key

--- frontend/src/components/Key.jsx
+++ frontend/src/components/Key.jsx
@@ -1,5 +1,20 @@
-const PVKey = () => {
-  return <div>Private Key!</div>;
+const PVKey = ({ pvk, setPvk }) => {
+  const handlePrivateKeyChange = (event) => {
+    const _key = event.target.value.trim();
+    setPvk(_key);
+  };
+
+  return (
+    <div>
+      <input
+        className="border border-gray-300 rounded w-2/3 px-2 py-1 mt-4"
+        type="password"
+        value={pvk}
+        placeholder="Enter your private key"
+        onChange={handlePrivateKeyChange}
+      />
+    </div>
+  );
 };
 
 export default PVKey;
